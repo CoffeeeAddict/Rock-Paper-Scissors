7c8dedc4759f433febcbf9dedb45395c346e62f3
CoffeeeAddict <184011415+CoffeeeAddict@users.noreply.github.com>
2025-01-25 14:43:38 -0600

    Added GetComputerChoice() Method
    Added method that randomly returns rock, paper, or scissors.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+for(let i=0; i<10; i++) {
+    console.log(GetComputerChoice());
+}
+
+function GetComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    switch(random) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
