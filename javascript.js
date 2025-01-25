db7cc95284a4f2a1ba99e923efa6089f2becb434
CoffeeeAddict <184011415+CoffeeeAddict@users.noreply.github.com>
2025-01-25 14:48:12 -0600

    Added Player Choice method
    Also added a method to check if a choice is valid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
-for(let i=0; i<10; i++) {
-    console.log(GetComputerChoice());
-}
+console.log(getHumanChoice());
 
-function GetComputerChoice() {
+function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     switch(random) {
         case 0:
@@ -14,5 +12,24 @@ function GetComputerChoice() {
         case 2:
             return "scissors";
             break;
+    }
+}
+
+function getHumanChoice() {
+    let choice = "";
+
+    while(!(isValidChoice(choice))) {
+        choice = prompt("Type your choice: rock, paper, or scissors");
+        choice = choice.toLowerCase();
+    }
+
+    return choice;
+}
+
+function isValidChoice(choice) {
+    if(choice == "rock" || choice == "paper" || choice == "scissors") {
+        return true;
+    } else {
+        return false;
     }
 }
