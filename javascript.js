bfe0ae5530438c4a765864e3013e13270cac44ab
CoffeeeAddict <184011415+CoffeeeAddict@users.noreply.github.com>
2025-02-08 10:44:48 -0600

    Added UI

--- javascript.js
+++ javascript.js
@@ -1,41 +1,80 @@
 
-playGame();
+const playButtons = document.querySelectorAll(".playButton");
+const humanScoreText = document.createElement("span");
+const computerScoreText = document.createElement("span");
+const resultText = document.querySelector(".results");
+document.querySelector(".playerScore").append(humanScoreText);
+document.querySelector(".computerScore").append(computerScoreText);
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    for(let i=0; i<5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
+humanScoreText.textContent = humanScore;
+computerScoreText.textContent = computerScore;
 
-        console.log(`Current Scores: Human: ${humanScore}, Computer: ${computerScore}`);
+playButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playRound(e);
+    })
+})
+
+
+function playRound(event) {
+    classes = event.target.classList;
+    let humanChoice = getHumanChoice(classes);
+    let computerChoice = getComputerChoice();
+    let outcome = findOutcome(humanChoice, computerChoice);
+    displayOutcome(humanChoice, computerChoice, outcome);
+    if(humanScore == 5 || computerScore == 5) {
+        gameFinish();
+        return;
     }
+}
 
-    if(computerScore > humanScore) {
-        console.log(`Computer wins!`);
-    } else if(humanScore > computerScore) {
-        console.log(`Human wins!`)
+function gameFinish() {
+    finalText = document.createElement("div");
+    finalText.classList.add("finalText");
+    if(humanScore > computerScore) {
+        finalText.textContent = "Player won!";
+    } else if(computerScore > humanScore) {
+        finalText.textContent = "Computer won!";
     } else {
-        console.log(`It's a tie!`);
+        finalText.textContent = "It's a tie!";
     }
+    resultText.append(finalText);
+    playButtons.forEach((button) => {
+        button.removeEventListener("click", (e) => {
+            playRound(e);
+        })
+    })
+}
 
-    function playRound(humanChoice, computerChoice) {
-        let outcome = findOutcome(humanChoice, computerChoice);
-    
-        switch(outcome) {
-            case "tie":
-                console.log(`It's a tie between your ${humanChoice} and the computer's ${computerChoice}!`);
-                break;
-            case "win":
-                console.log(`You win! Your ${humanChoice} beats the computer's ${computerChoice}!`);
-                humanScore++;
-                break;
-            case "lose":
-                console.log(`You lose! The computer's ${computerChoice} beats your ${humanChoice}!`);
-                computerScore++;
-        }
+function getHumanChoice(classes) {
+    if(classes.contains("rock")) {
+        return "rock";
+    } else if(classes.contains("paper")) {
+        return "paper";
+    } else if(classes.contains("scissors")) {
+        return "scissors;"
+    }
+}
+
+
+
+function displayOutcome(humanChoice, computerChoice, outcome) {
+    switch(outcome) {
+        case "tie":
+            resultText.textContent = `It's a tie between your ${humanChoice} and the computer's ${computerChoice}!`;
+            break;
+        case "win":
+            resultText.textContent = `You win! Your ${humanChoice} beats the computer's ${computerChoice}!`;
+            humanScore++;
+            humanScoreText.textContent = humanScore;
+            break;
+        case "lose":
+            resultText.textContent = `You lose! The computer's ${computerChoice} beats your ${humanChoice}!`;
+            computerScore++;
+            computerScoreText.textContent = computerScore;
     }
 }
 
@@ -72,18 +111,6 @@ function getComputerChoice() {
             return "scissors";
             break;
     }
-}
-
-function getHumanChoice() {
-    let choice = "";
-
-    while(!(isValidChoice(choice))) {
-        choice = prompt("Type your choice: rock, paper, or scissors");
-
-        choice = choice.toLowerCase();
-    }
-
-    return choice;
 }
 
 function isValidChoice(choice) {
